10111d7f368fae4734ef27aae7f2bf4990f43018
Aleix Conchillo Flaque <aleix@member.fsf.org>
2009-12-29 02:14:50 +0100

    update news and setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,16 @@ setup(name = 'BitPacket',
       description = 'A Python object-oriented representation of packets',
       long_description =
       '''
-      This module provides a simple objected-oriented representation
-      of packets. The main purpose is to provide an easy and
-      extensible interface for building and parsing packets.
+      BitPacket is a Python module that provides a simple
+      objected-oriented representation of packets. The main purpose is
+      to provide an easy and extensible interface for building and
+      parsing packets.
+
+      These are the main features of BitPacket:
+
+        * Packets can be packed/unpacked to/from bytes.
+        * Allows meta-fields, that is, fields that depend on other fields.
+        * A packet can have single bit fields.
       ''',
       classifiers = [
         'Development Status :: 5 - Production/Stable',
