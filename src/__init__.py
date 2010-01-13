b593740086e7b0831d96435e741cab85c9cbe6b5
Aleix Conchillo Flaque <aleix@member.fsf.org>
2010-01-13 18:56:43 +0100

    allow import global module

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,54 @@
-# Dummy file to load BitPacket package
+#
+# @file    __init__.py
+# @brief   An object-oriented representation of bit field structures
+# @author  Aleix Conchillo Flaque <aleix@member.fsf.org>
+# @date    Wed Jan 13, 2010 18:46
+#
+# Copyright (C) 2010 Aleix Conchillo Flaque
+#
+# This file is part of BitPacket.
+#
+# BitPacket is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BitPacket is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BitPacket.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from BitField import BitField
+from BitStructure import BitStructure
+from Container import Container
+from Field import Field
+from Integer import *
+from MetaData import MetaData
+from MetaStructure import MetaStructure
+from Real import *
+from Value import Value
+from Writer import Writer
+from WriterBasic import WriterBasic
+from WriterTable import WriterTable
+from WriterXML import WriterXML
+
+__all__ = [ 'BitField',
+            'BitStructure',
+            'Container',
+            'Field',
+            'Int8', 'UInt8',
+            'Int16', 'UInt16',
+            'Int32', 'UInt32',
+            'Int64', 'UInt64',
+            'MetaData',
+            'MetaStructure',
+            'Float', 'Double',
+            'Value',
+            'Writer',
+            'WriterBasic',
+            'WriterTable',
+            'WriterXML' ]
