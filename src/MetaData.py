37aab4c4a1e568bff2d6b596013d0dc943555819
Aleix Conchillo Flaque <aleix@member.fsf.org>
2010-01-26 18:28:09 +0100

    MetaData: use reference to self instead of context

--- src/MetaData.py
+++ src/MetaData.py
@@ -31,7 +31,7 @@ class MetaData(Structure):
     def __init__(self, name, lengthtype, wsizefunc):
         Structure.__init__(self, name)
         self.__length = lengthtype("Length")
-        self.__data = String("Data", 
-                             lengthfunc = lambda ctx: ctx["Length"] * wsizefunc(ctx))
+        self.__data = String("Data",
+                             lengthfunc = lambda ctx: self["Length"] * wsizefunc(ctx))
         self.append(self.__length)
         self.append(self.__data)
