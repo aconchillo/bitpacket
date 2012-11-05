4c7c61fc3beeb22263c2fe7356d018b9d3ac3979
Aleix Conchillo Flaque <aconchillo@gmail.com>
2012-11-04 17:09:39 -0800

    minor Array documentation improvements

--- src/BitPacket/Array.py
+++ src/BitPacket/Array.py
@@ -32,14 +32,14 @@ __doc__ = '''
 
     **API reference**: :class:`Array`
 
-    Sometimes we need to create packets th  at have a number of repeated
+    Sometimes we need to create packets that have a number of repeated
     fields in it. Normally, these kind of packets have a counter field
     indicating the number of repeated fields after it.
 
-    An *Array* is a subclass of *Structure*. Initially, it contains a
-    length field, which is the one that will indicate how many fields
-    the array holds. The type of the length field is specified in the
-    *Array* constructor.
+    An :mod:`Array` is a subclass of :mod:`Structure`. Initially, it
+    contains a length field, which is the one that will indicate how
+    many fields the array holds. The type of the length field is
+    specified in the *Array* constructor.
 
     +--------+--------+---------+
     | count  |   id   | address |
@@ -49,9 +49,9 @@ __doc__ = '''
     |        |  *count* times   |
     +--------+------------------+
 
-    In order to create an *Array* for the depicted packet above, we can
-    define the base type of the fields (all of the same type) that this
-    array will contain. We will create a *MyStructure* class that
+    In order to create an :mod:`Array` for the depicted packet above, we
+    can define the base type of the fields (all of the same type) that
+    this array will contain. We will create a *MyStructure* class that
     contains two fields, *id* and *address*.
 
     >>> class MyStructure(Structure):
@@ -60,18 +60,18 @@ __doc__ = '''
     ...        self.append(UInt8("id", id))
     ...        self.append(UInt32("address", address))
 
-    Now, we can define an *Array* that contains the default counter
-    field of our desired size and a single parameter function that tells
-    how to create the array fields.
+    Now, we can define an :mod:`Array` that contains the default counter
+    field of our desired name and size and a single argument function
+    that tells how to create the array fields.
 
-    >>> packet = Array("mypacket", UInt8("counter"), MyStructure)
+    >>> packet = Array("mypacket", UInt8("counter"),
+    ...                lambda root: MyStructure())
 
     As a second argument to the *Array* constructor, we specify the
     field that specifies how many elements the array contains. As the
-    third argument, we have provided the type of the array elements. The
-    anonymous function takes an argument which is the root of the packet
-    we are building/parsing (this means we have direct access to all
-    other packet fields).
+    third argument, we have provided how to create the array
+    elements. The anonymous function takes an argument which is the
+    top-level root :mod:`Container` that the Array belongs to.
 
     So, let's try to unpack some data and see what happens:
 
@@ -87,9 +87,9 @@ __doc__ = '''
     At this point the array contains one field of type *MyStructure* as
     it has unpacked the given array, seen that the *counter* field had
     value 1 and therefore read one *MyStructure* field. It is worth
-    noting that the fields added to an *Array* are automatically named
-    in a zero-based scheme. That is, to access the first *address* field
-    value we could:
+    noting that the fields added to an :mod:`Array` are automatically
+    named in a zero-based scheme. That is, to access the first *address*
+    field value we could do:
 
     >>> packet["0.address"]
     270544960
@@ -97,7 +97,8 @@ __doc__ = '''
     We can also easily add some data to the array. Consider again our
     packet:
 
-    >>> packet = Array("mypacket", UInt8("counter"), MyStructure)
+    >>> packet = Array("mypacket", UInt8("counter"),
+    ...                lambda root: MyStructure())
 
     Adding a *MyStructure* field is as easy as adding a field to any
     other *Structure*:
@@ -110,12 +111,22 @@ __doc__ = '''
         (id = 54)
         (address = 98812383)))
 
+    It is interesting to see that if we add something else that is not a
+    *MyStructure*, a *TypeError* exception will be raised notifying
+    about the problem:
 
-    Complex meta structures
-    -----------------------
+    >>> try:
+    ...   packet.append(UInt8("wrong", 12))
+    ... except TypeError as err:
+    ...   print "Error: %s" % err
+    Error: Invalid field type for array 'mypacket' (expected <class 'MyStructure'>, got <class 'BitPacket.Integer.UInt8BE'>)
+
+
+    Complex arrays
+    --------------
 
     We can also build more complex packets, such as the one below, where
-    we have one *Array* inside another.
+    we have one :mod:`Array` inside another.
 
     +--------+--------+--------+----------------+
     | count1 |   id   | count2 |     address    |
@@ -128,9 +139,9 @@ __doc__ = '''
     +--------+----------------------------------+
 
     We will first create a structure for the list of addresses. It will
-    contain the *count2* counter and an *Array* whose number of elements
-    is provided by *count2* and that will be filled with 32-bit unsigned
-    integers.
+    contain the *count2* counter and an :mod:`Array` whose number of
+    elements is provided by *count2* and that will be filled with 32-bit
+    unsigned integers.
 
     >>> class AddressList(Structure):
     ...     def __init__(self):
@@ -138,20 +149,21 @@ __doc__ = '''
     ...         self.append(UInt8("id"))
     ...         self.append(Array("address",
     ...                           UInt8("count2"),
-    ...                           lambda ctx: UInt32("value")))
+    ...                           lambda root: UInt32("value")))
 
     Now, we can build our packet as an structure with the *count1*
-    counter and an *Array* whose number of elements is provided by
+    counter and an :mod:`Array` whose number of elements is provided by
     *count1* and that will be filled by address lists (that, remember,
-    already has another *Array*).
+    already has another :mod:`Array`).
 
     >>> s = Array("mypacket",
     ...           UInt8("count1"),
-    ...           lambda ctx: AddressList())
+    ...           lambda root: AddressList())
 
     So, let's try to set some data to this packet. As we have seen
-    before with the simplest case, data should be propagated and *Array*
-    meta properties will be used to build the desired fields.
+    before with the simplest case, data should be propagated and
+    :mod:`Array` meta properties will be used to build the desired
+    fields.
 
     >>> s.set_array(array.array("B", [0x02, # count1
     ...                               0x01, # id (1)
@@ -176,7 +188,7 @@ __doc__ = '''
           (0 = 84281096)
           (1 = 151653132))))
 
-    It worked! As we see, our packet consists on a *mystructure* that
+    It works! As we see, our packet consists of a *mystructure* that
     contains two *AddressList* fields. The first one with a single
     address and the second with two.
 
@@ -254,36 +266,3 @@ class Array(Structure):
             raise IndexError("Index %s must be <= %s" % (names[0], length))
 
         Structure.__setitem__(self, name, value)
-
-# from BitPacket.Integer import *
-# from BitPacket.MetaField import *
-
-# import array
-
-# class Sub32(UInt32):
-
-#     def __init__(self, value):
-#         UInt32.__init__(self, "foo", value)
-
-# a = Array("test", UInt8("len"), lambda ctx: UInt32("test"))
-# a.append(UInt32("value", 23))
-# a.append(Sub32(34))
-
-# a.set_array(array.array("B", [2, 0, 0, 0, 23, 0, 0, 0, 34]))
-
-# print a
-
-# ma = Array("memory", UInt8,
-#            lambda ctx: MetaField("metaaddress",
-#                                  lambda ctx: Array("address", UInt8,
-#                                                    lambda ctx: MetaField("metavalue", lambda ctx: UInt32("value")))))
-
-# address = Array("address", UInt8, UInt32)
-# address.append(UInt32("0", 0x12345678))
-# address.append(UInt32("1", 0x9ABCDEFF))
-# ma.append(address)
-# ma.set_array(array.array("B", [1,
-#                                2,
-#                                0x12, 0x34, 0x56, 0x78,
-#                                0x9A, 0xBC, 0xDE, 0xFF]))
-# print ma
