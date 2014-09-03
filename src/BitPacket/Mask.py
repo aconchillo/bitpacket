e7ef0ddab3862af5baad5b6767dc6430134c98b8
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2014-09-02 23:43:18 -0700

    mask: implement mask as a bitfield
    * src/BitPacket/Mask.py: instead of using struct and only allow 8, 16,
      32 and 64 bit masks, we use BitFields and make bit masks the size we
      need.

--- src/BitPacket/Mask.py
+++ src/BitPacket/Mask.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 #
 # @file    Mask.py
-# @brief   An object-oriented representation of bit field structures
+# @brief   A field to represent bit masks
 # @author  Aleix Conchillo Flaque <aconchillo@gmail.com>
 # @date    Wed Nov 17, 2010 13:02
 #
-# Copyright (C) 2010, 2011, 2012 Aleix Conchillo Flaque
+# Copyright (C) 2010, 2011, 2012, 2013, 2014 Aleix Conchillo Flaque
 #
 # This file is part of BitPacket.
 #
@@ -23,39 +23,74 @@
 # along with BitPacket.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from BitPacket.Value import *
+__doc__ = '''
+
+    Masks
+    =====
+
+    A field to represent bit masks.
+
+    **API reference**: :class:`Mask`
+
+    A bit mask is a field where each bit has a different
+    meaning. Multiples bits can be set or unset at once, but each one
+    will represent a single thing. Bit masks are commonly used to define
+    a set of options.
+
+    For example, we can define a 2-bit mask of whether our packet
+    includes audio, video or both:
+
+    +-------------+
+    | audio/video |
+    +=============+
+    |   2 bits    |
+    +-------------+
+
+    This can be simply constructed by the following code:
+
+    >>> m = Mask("audio/video", 0, AUDIO = 0x01, VIDEO = 0x02)
+    >>> m.mask(m.AUDIO | m.VIDEO)
+    >>> print m
+    (audio/video = 0x03)
+
+    We can also unmask a single option:
+
+    >>> m.unmask(m.AUDIO)
+    >>> print m
+    (audio/video = 0x02)
+
+    Or unmask all of them with *set_value*:
+
+    >>> m.set_value(0)
+    >>> print m
+    (audio/video = 0x00)
+
+'''
+
 from BitPacket.BitField import *
 
+from math import log
 from operator import itemgetter
-
-__STRUCT_MASK8_FMT__ = ">B"
-__STRUCT_MASK16_FMT__ = ">H"
-__STRUCT_MASK32_FMT__ = ">I"
-__STRUCT_MASK64_FMT__ = ">Q"
-
-__MASK_STR__ = ["Unmasked", "Masked"]
-
 
 class Mask(BitField):
 
-    def __init__(self, name, value = False):
-        BitField.__init__(self, name, 1, value)
+    '''
+    This class represents a bit mask field. Each bit has a unique
+    meaning. Bit masks can be set, unset and tested.
+    '''
 
-    def str_value(self):
-        return __MASK_STR__[self.value()]
+    def __init__(self, name, value, **kwargs):
+        '''
+        Initialize the field with the given *name* and *value*. The list of
+        all masks and their values are be given in *kwargs* with an
+        arbitrary number of arguments (FLAG_1 = 0x01, FLAG_2 = 0x02
+        ...).
 
-    def str_eng_value(self):
-        return __MASK_STR__[self.eng_value()]
+        '''
+        BitField.__init__(self, name, len(kwargs.items()), value)
 
-
-class MaskValue(Value):
-
-    def __init__(self, name, format, masktype, value, **kwargs):
-        Value.__init__(self, name, format, value)
-
+        # List of masks in order (by value).
         self.__masks = []
-        self.__fields = []
-        self.__fields_dict = {}
 
         # Add bit masks. Note that kwargs is a dictionary, hence not
         # ordered.
@@ -64,75 +99,57 @@ class MaskValue(Value):
             self.__masks.append(mask[0])
             self.__dict__[mask[0]] = mask[1]
 
-            field = masktype(mask[0])
-            field._set_parent(self)
-            field._set_root(self.root())
-            self.__fields.append(field)
-            self.__fields_dict[mask[0]] = field
-
-        self.__update_masks()
+    def is_set(self, mask):
+        '''
+        Tests whether the given bit *mask* is set. That is, all the given
+        bits must be set.
+        '''
+        return (self.value() | mask)
 
     def mask(self, mask):
+        '''
+        Mask the given bits in *mask*. The current bits will be kept and
+        the new ones will be additionally masked.
+        '''
         self.set_value(self.value() | mask)
-        self.__update_masks()
 
     def unmask(self, mask):
+        '''
+        Unmask the given bits in *mask*. The current bits will be kept and
+        the new ones will be additionally unmasked.
+        '''
         self.set_value(self.value() & ~mask)
-        self.__update_masks()
 
-    def __update_masks(self):
-        for mask in self.__masks:
-            active = ((self.value() & self.__dict__[mask]) != 0)
-            self.__fields_dict[mask].set_value(active)
+    def set_value(self, value):
+        '''
+        Resets the current mask with the given bits in *value*. If *value*
+        doesn't fit in this field mask an exception will be raised.
 
-    def fields(self):
-        return self.__fields
+        '''
+        size = self.size()
+        if value > 0:
+            size = int(log(value, 2) + 1)
+        if size <= self.size():
+            BitField.set_value(self, value)
+        else:
+            raise ValueError("Value bit size is bigger than the mask size "
+                             "(value %d has size %d, mask size is %d)"
+                             % (value, size, self.size ()))
 
     def str_value(self):
+        '''
+        Returns a human-readable representation of the hexadecimal value
+        of this field.
+        '''
         return self.str_hex_value()
 
     def str_eng_value(self):
-        return self.str_hex_value()
-
-
-class Mask8(MaskValue):
-
-    def __init__(self, name, masktype, value = 0, **kwargs):
-        MaskValue.__init__(self, name, __STRUCT_MASK8_FMT__, masktype,
-                           value, **kwargs)
-
-class Mask16(MaskValue):
-
-    def __init__(self, name, masktype, value = 0, **kwargs):
-        MaskValue.__init__(self, name, __STRUCT_MASK16_FMT__, masktype,
-                           value, **kwargs)
-
-class Mask32(MaskValue):
-
-    def __init__(self, name, masktype, value = 0, **kwargs):
-        MaskValue.__init__(self, name, __STRUCT_MASK32_FMT__, masktype,
-                           value, **kwargs)
-
-class Mask64(MaskValue):
-
-    def __init__(self, name, masktype, value = 0, **kwargs):
-        MaskValue.__init__(self, name, __STRUCT_MASK64_FMT__, masktype,
-                           value, **kwargs)
-
-
-# from BitPacket.Boolean import *
-# from BitPacket.Flag import *
-
-# a =  Mask8("ValidityFlags", Mask, 0,
-#            FLAG_1 = 0x01,
-#            FLAG_2 = 0x02,
-#            FLAG_3 = 0x04,
-#            FLAG_4 = 0x08,
-#            FLAG_5 = 0x10,
-#            FLAG_6 = 0x20,
-#            FLAG_7 = 0x40,
-#            FLAG_8 = 0x80)
-
-# a.mask(a.FLAG_1 | a.FLAG_2)
-
-# print a
+        '''
+        Returns a text string with all the bit mask names that are set. Bit
+        masks names are separated by *|*.
+        '''
+        masks = []
+        for mask in self.__masks:
+            if self.value() & self.__dict__[mask]:
+                masks.append(mask)
+        return "|".join(masks)
