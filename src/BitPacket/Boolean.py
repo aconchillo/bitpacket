a7440cf5a82e5e558665d6f016abb04614cdb0cc
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2014-09-03 23:02:55 -0700

    Boolean: added documentation

--- src/BitPacket/Boolean.py
+++ src/BitPacket/Boolean.py
@@ -5,7 +5,7 @@
 # @author  Aleix Conchillo Flaque <aconchillo@gmail.com>
 # @date    Wed Nov 17, 2010 13:02
 #
-# Copyright (C) 2010, 2011 Aleix Conchillo Flaque
+# Copyright (C) 2010, 2011, 2012, 2013, 2014 Aleix Conchillo Flaque
 #
 # This file is part of BitPacket.
 #
@@ -25,9 +25,30 @@
 
 __doc__ = '''
 
+    Booleans
+    ========
+
     A bit field for representing a boolean type.
 
     **API reference**: :class:`Boolean`
+
+    A :class:`Boolean` is a :class:`BitField` with a single bit. So only
+    True or False can be set.
+
+    >>> b = Boolean("Option", True)
+    >>> print b
+    (Option = True)
+
+    There a couple of helper function to enable or disable the field
+    value:
+
+    >>> b.disable()
+    >>> print b
+    (Option = False)
+
+    >>> b.enable()
+    >>> print b
+    (Option = True)
 
 '''
 
@@ -38,17 +59,38 @@ __BOOLEAN_STR__ = ["False", "True"]
 
 class Boolean(BitField):
 
+    '''
+    This class represents a boolean field. It is a convenient class to
+    represent True and False values.
+    '''
+
     def __init__(self, name, value = False):
+        '''
+        Initialize the field with the given *name*. The default value is
+        False.
+        '''
         BitField.__init__(self, name, 1, value)
 
     def enable(self):
+        '''
+        Set the value to True.
+        '''
         self.set_value(True)
 
     def disable(self):
+        '''
+        Set the value to False.
+        '''
         self.set_value(False)
 
     def str_value(self):
+        '''
+        Returns a text string with True or False.
+        '''
         return __BOOLEAN_STR__[self.value()]
 
     def str_eng_value(self):
+        '''
+        Returns a text string with True or False.
+        '''
         return __BOOLEAN_STR__[self.eng_value()]
